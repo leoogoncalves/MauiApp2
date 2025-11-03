5bc748eedd6c384723f95a82695e133bc326980b
Leonardo <leonardogoncnascimento@gmail.com>
2025-11-02 22:59:59 -0300

    Add recurso ao App

--- MauiApp2/Views/ContartacaoHospedagem.xaml.cs
+++ MauiApp2/Views/ContartacaoHospedagem.xaml.cs
@@ -2,13 +2,41 @@ namespace MauiApp2.Views;
 
 public partial class ContartacaoHospedagem : ContentPage
 {
-	public ContartacaoHospedagem()
-	{
-        InitializeComponent();
-	}
+    App PropriedadesApp;
 
-    private async void Button_Clicked(object sender, EventArgs e)
+    public ContartacaoHospedagem()
     {
-        await Navigation.PushAsync(new sobre());
+        InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+
+        } catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+    }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+
     }
 }
--- MauiApp2/Views/sobre.xaml.cs
+++ MauiApp2/Views/sobre.xaml.cs
@@ -7,5 +7,18 @@ namespace MauiApp2.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PopAsync();
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
     }
 }
